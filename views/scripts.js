049484ad95d1eb761d359da035d6b772dc7706e2
Debrena McEwen <debrenamcewen@gmail.com>
2017-05-15 16:20:25 -0700

    add views to calculator

---
+++ views/scripts.js
@@ -0,0 +1,10 @@
+(function() {
+
+})()
+
+
+// var calculator = new Calculator()
+// calculator.compute(5, 4, '*')
+// console.log(calculator.result()) // 20
+// calculator.compute()
+// console.log(calculator.result()) // 80
